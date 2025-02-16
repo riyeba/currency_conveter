ad762a0723b98892b4008e7b20b64e22dac5c406
Akinpennu Taiwo <moriyebas@gmail.com>
2025-02-17 00:51:21 +0300

    4th commit

--- src/components/Sender.jsx
+++ src/components/Sender.jsx
@@ -31,7 +31,7 @@ function Sender() {
   return (
     
     <div className="h-screen flex flex-col items-center justify-center bg-gray-100 p-6 ">
-      {loading1? "Please wait" : <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-md">
+      {loading1? "Please wait" : <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-md mt-[-60px]">
         <h1 className="text-xl font-semibold text-gray-800 text-center mb-4">
           Naira to SAR Exchange Rate
         </h1>
